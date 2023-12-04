18b8af46d8f5f3f021e5cb21a88bde1f5d179b0e
brunoeduardodev <brunoeduardo.dev@gmail.com>
2023-12-03 21:16:47 -0300

    feat: add testing to find product by id

--- products_test.go
+++ products_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/brunoeduardodev/go-raw-sql/internal/helpers"
 	"github.com/brunoeduardodev/go-raw-sql/internal/services/products"
+	"github.com/brunoeduardodev/go-raw-sql/repositories"
 )
 
 func TestProducts(t *testing.T) {
@@ -22,6 +23,62 @@ func TestProducts(t *testing.T) {
 				if len(response.Products) > 0 {
 					t.Errorf("expected empty list of products, but got %v", response.Products)
 				}
+			},
+		},
+		{
+			Name:               "Should be able to create a product",
+			Method:             "POST",
+			Route:              "products",
+			ExpectedStatusCode: 200,
+			Body: repositories.CreateProductInput{
+				Name:  "Product 1",
+				Price: 10000,
+			},
+			TestResponse: func(decoder *json.Decoder) {
+				var response products.CreateProductResponse
+				helpers.EnsureResponseDecodesTo(t, decoder, &response)
+
+				if response.Product.Name != "Product 1" && response.Product.Price != 10000 {
+					t.Errorf("Expected receive product with name \"Product 1\" and price 10000 but got Name=%s, Price=%d", response.Product.Name, response.Product.Price)
+				}
+			},
+		},
+		{
+			Name:               "Should be able to list created product",
+			Method:             "GET",
+			Route:              "products",
+			ExpectedStatusCode: 200,
+			TestResponse: func(decoder *json.Decoder) {
+				var response products.ListProductsResponse
+				helpers.EnsureResponseDecodesTo(t, decoder, &response)
+
+				if len(response.Products) != 1 {
+					t.Errorf("Expected to receive only 1 product, but got %d", len(response.Products))
+				}
+
+				if response.Products[0].Name != "Product 1" {
+					t.Errorf("Expected to receive a product with name \"Product 1\", but got %s", response.Products[0].Name)
+				}
+
+			},
+		},
+		{
+			Name:               "Should be able to find created product by id",
+			Method:             "GET",
+			Route:              "products/1",
+			ExpectedStatusCode: 200,
+			TestResponse: func(decoder *json.Decoder) {
+				var response products.FindProductByIdResponse
+				helpers.EnsureResponseDecodesTo(t, decoder, &response)
+
+				if response.Product.Id != 1 {
+					t.Errorf("Expected to find product with id 1 but got %d", response.Product.Id)
+				}
+
+				if response.Product.Name != "Product 1" {
+					t.Errorf("Expected to receive a product with name \"Product 1\", but got %s", response.Product.Name)
+				}
+
 			},
 		},
 	}
